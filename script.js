fa4276a8c202ce0648e0b15bde3a74b64fcbeebd
Pg Network <gamerzp780@gmail.com>
2024-10-29 20:24:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ document.getElementById('theme-toggle').addEventListener('click', () => {
     document.body.classList.toggle('light-theme');
     document.getElementById('theme-toggle').textContent = document.body.classList.contains('dark-theme') ? "🌙" : "☀️";
 });
+
